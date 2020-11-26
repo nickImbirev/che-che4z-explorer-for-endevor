fd02a6445266ed8fc7674a1d8cc62c3d6b53ec0a
Nikolai Imbirev <nikolay.imbirev@broadcom.com>
2020-11-26 20:48:30 +0100

    add unit test for browse command

---
+++ src/__tests__/__unit__/commands/browseElement-test.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2020 Broadcom.
+ * The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Broadcom, Inc. - initial API and implementation
+ */
+
+import * as vscode from 'vscode';
+import { browseElement } from '../../../commands/BrowseElement';
+
+// Explicitly show NodeJS how to find VSCode (required for Jest)
+process.vscode = vscode;
+
+describe('process for submitting browse element command', () => {
+    it('should submit uri request to content provider', async () => {
+        // given
+        const uriMock: vscode.Uri = {
+            scheme: 'some_scheme',
+            authority: 'some_authority',
+            fsPath: '/path',
+            path: '/path',
+            query: 'some_value',
+            fragment: 'very_important_fragment',
+            with: jest.fn(),
+            toJSON: jest.fn()
+        };
+        vscode.window.showTextDocument = jest.fn();
+        // when
+        await browseElement(uriMock);
+        // then
+        const inActiveWorkspace = { preview: false };
+        expect(vscode.window.showTextDocument).toHaveBeenCalledWith(uriMock, inActiveWorkspace);
+    });
+});
